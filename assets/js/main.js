2ffff23d7b0956cde9de4ed39b7332b24ef21f49
Songyao Jiang <38265584+songyaojiang@users.noreply.github.com>
2019-01-09 21:44:23 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,7 +267,6 @@
                 var $image_img = $a.children('img');
                 var data = exifDatas[$image_img.data('name')];
                 if (data === undefined || data === "") {
-                    console.log("data undefined")
                     // EXIF data					
                     EXIF.getData($image_img[0], function () {
                         data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
